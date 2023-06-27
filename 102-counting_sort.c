2d8c20fc40bfbeced05e4f082e2f129a93c99342
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-06-28 01:08:34 +0200

    6. Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,15 +9,13 @@ void counting_sort(int *array, size_t size)
 {
 	int max = array[0];
 	size_t index = 0;
-    int *count = malloc((max + 1) * sizeof(int));
+	int *count = malloc((max + 1) * sizeof(int));
 
 	for (size_t i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
-
-	/* Create a counting array of size max + 1 and initialize all elements to 0 */
 	if (count == NULL)
 	{
 		printf("Memory allocation failed\n");
@@ -27,20 +25,14 @@ void counting_sort(int *array, size_t size)
 	{
 		count[i] = 0;
 	}
-
-	/* Count the occurrences of each element in the array */
 	for (size_t i = 0; i < size; i++)
 	{
 		count[array[i]]++;
 	}
-
-	/* Print the counting array */
 	for (int i = 0; i <= max; i++)
 	{
 		printf("%d, ", count[i]);
 	} printf("\n");
-
-	/* Update the array with the sorted elements */
 	for (int i = 0; i <= max; i++)
 	{
 		while (count[i] > 0)
